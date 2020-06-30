dc72f8da3e0ac93c0fe4861cc4342bd757dda19c
SBarton2005 <sbarton2024@k12.andoverma.us>
2020-06-30 12:51:54 +0000

    Tried something else. Next time just try a right triangle

--- main.py
+++ main.py
@@ -4,26 +4,24 @@ t = turtle.Turtle()
 def roof():
   width = int(input("Roof Width: "))
   height = int(input("Roof Height: "))
-  color = input("Roof Color: ")
   leg = width / 2
-  hyp = ((leg ** 2) + (height ** 2)) ** .5
-  a = math.degrees(math.atan(height/leg))
-  b = 2*(math.degrees(math.atan(leg/height)))
-  c = a
-  if a + b/2 != 90:
-    print("The degrees don't add up!")
-    print(a, b)
-    quit()
-  t.fillcolor(color)
-  t.begin_fill()
+  hyp = math.sqrt(leg ** 2 + height ** 2)
+  print(leg, height, hyp)
+  a = height ** 2 + hyp ** 2 - leg ** 2
+  b = 2 * height * hyp
+  angle1 = math.degrees(math.acos(a/b))
+  c = leg ** 2 + hyp ** 2 - height ** 2
+  d = 2 * leg * hyp
+  angle2 = 90-angle1
+  angle2 *= 2
   t.forward(width)
-  t.left(180)
-  t.right(a)
-  t.forward(leg)
-  t.left(180)
-  t.right(b)
-  t.forward(leg)
-  t.left(180)
-  t.right(c)
-  t.end_fill()
+  t.right(180)
+  t.right(angle1)
+  t.forward(hyp)
+  t.right(180)
+  t.right(angle2)
+  t.forward(hyp)
+  t.right(180)
+  t.right(angle1)
+  print(angle1, angle2)
 roof()
